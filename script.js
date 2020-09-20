bfd226af089b46de408ff36ef7f730f09003aff2
BeTheICON_AneTh <ane@betheicon.no>
2020-09-20 13:30:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+// question 1.
+// declaring variable firstVar without initializing it
+var firstVar;
+console.log(firstVar);
+
+// question 2.
+// declaring variable name assigning it the value my first name
+var name;
+name = "Ane";
+console.log(name);
+
+// question 3.
+// declaring and initializing the variable numberValue initializing it with the value 2
+var numberValue = 2;
+console.log(numberValue);
+
+// question 4.
+// creating variable division and initializing it with a value of 20 divided by 5
+var division = 20 / 5;
+console.log(division);
+
+// question 5.
+// write code checking the type of the value "frog"
+
+var animal = "frog";
+console.log(animal);
+
+typeof animal;
+// "string"
+
+// question 6.
+// declearing and initializing variable "orderHasShipped" with bolean value
+
+var orderHasShipped = true;
+
+// question 7.
+/* creating if statement checking if "orderHasShipped" is true
+If true, console logging string value "true", if not console logging
+string value "false" */
+
+if (orderHasShipped === true) {
+	console.log("true");
+} else {
+	console.log("false");
+}
+
+// question 8.
+/* creating a for loop counting from 0 to 9.
+console logging value of the counter variable inside the loop*/
+
+for (var i = 0; i < 10; i++) {
+	console.log(i);
+}
